074ecb084755b2084e5b6eeac90ddc0875c5d312
MAROIS_KEVIN_2\kevin <kevin@maroisconsulting.com>
2024-08-10 21:20:53 -0700

    Add project files.

---
+++ MyToggleButton/MyToggleButton.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyToggleButton
+{
+    public class MyToggleButton : ToggleButton
+    {
+        public static ComponentResourceKey ButtonBrushKey = 
+            new ComponentResourceKey(typeof(MyToggleButton), "ButtonBrush");
+
+        static MyToggleButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(MyToggleButton), 
+                new FrameworkPropertyMetadata(typeof(MyToggleButton)));
+        }
+    }
+}
